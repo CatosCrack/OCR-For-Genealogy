31546532405eec96c00c9b4abd3ab0068338580f
Camilo G <cgalvis.sierra@gmail.com>
2024-12-24 20:10:25 -0500

    Added functionality to get data from database and display the results dynamically on a list

---
+++ website/public/display_results.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    // Get data from sessionStorage
+    const results = JSON.parse(sessionStorage.getItem('results'));
+
+    // Get result container to add result components
+    const container = document.getElementById('results_list');
+
+    // Loop through each result and add it to the container
+    results.forEach(result => {
+        var i = 1;
+
+        // Create result
+        const resultRow = document.createElement('row');
+        resultRow.className = "row d-flex justify-content-center align-items-center mt-4 mb-4";
+        resultRow.id = "result";
+        resultRow.innerHTML = `
+            <!-- Individual Result -->
+                <!-- Image -->
+                <div class="col-4 d-flex" id="result_image">
+                    <img src="images/1.jpg">
+                    <a href=${result["url"]}><button id="view_button">Abrir documento</button></a>
+                </div>
+                <!-- Data -->
+                <div class="col-8 d-flex flex-column" id="data">
+                    <div class="row mb-4">
+                        <h3><a href=${result["url"]} style="color: #173753;">${result["name"]}</a></h3>
+                    </div>
+                    <!-- Municipality Row -->
+                    <div class="row">
+                        <div class="col-4">
+                            <h4>Municipio</h4>
+                        </div>
+                        <div class="col-8">
+                            <p>${result["municipality"]}</p>
+                        </div>
+                    </div>
+                    <!-- Department Row -->
+                    <div class="row">
+                        <div class="col-4">
+                            <h4>Departamento</h4>
+                        </div>
+                        <div class="col-8">
+                            <p>${result["department"]}</p>
+                        </div>
+                    </div>
+                    <!-- Year Row -->
+                    <div class="row">
+                        <div class="col-4">
+                            <h4>Año</h4>
+                        </div>
+                        <div class="col-8">
+                            <p>${result["year"]}</p>
+                        </div>
+                    </div>
+                </div>
+        `;
+        container.appendChild(resultRow);
+    });
+});

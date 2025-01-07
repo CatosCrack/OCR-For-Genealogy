392dab267aadd942e38339bee393929775c0af4b
Camilo G <cgalvis.sierra@gmail.com>
2025-01-06 20:57:40 -0500

    made changes to the processed data to remove names that also appeared as last names. This is to increase the accuracy of the synthetic data generation

--- ner_model/data/process_files.py
+++ ner_model/data/process_files.py
@@ -2,6 +2,7 @@
 
 import csv
 import os
+import pandas as pd
 
 # Get a list of files in the current directory
 current_dir = f"{os.getcwd()}/raw/"
@@ -19,8 +20,30 @@ for filename in files:
                 text = row[0].split()
                 for i in range(len(text)):
                     text[i]=text[i].capitalize()
-                content.append(" ".join(text))
+                content.append([" ".join(text)])
         with open(f"{os.getcwd()}/processed/{filename[:-4]}_processed.csv", "w", newline="") as file:
             writer = csv.writer(file)
-            for row in content:
-                writer.writerow([row])
+            writer.writerows(content)
+
+# Update directories
+current_dir = f"{os.getcwd()}/processed/"
+
+# Remove first names from last name documents
+with open(f"{current_dir}first_names_processed.csv") as f_file, open(f"{current_dir}last_names_processed.csv") as l_file:
+    content = []
+    f_reader = list(csv.reader(f_file))
+    l_reader = csv.reader(l_file)
+    for l_row in l_reader:
+        flag = True
+        for f_row in f_reader:
+            if l_row[0] == f_row[0]:
+                flag = False
+                break
+        if flag:
+            content.append(l_row)
+            
+with open(f"{current_dir}last_names_processed.csv", "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(content)
+
+
